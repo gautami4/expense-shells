4c6d761a11f29399a74cf92f0e419965aeac6d3b
gautami4 <gowthami.it4@gmail.com>
2026-07-13 21:21:21 +0530

    sql

--- mysql.sh
+++ mysql.sh
@@ -16,11 +16,6 @@ LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 mkdir -p $LOGS_FOLDER 
 
-if [ $? -ne 0 ]
-then
-    echo " The directory is already created"
-fi
-
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
@@ -50,4 +45,13 @@ VALIDATE $? "Enabling MySQL Server"
 systemctl start mysqld &>>$LOG_FILE_NAME
 VALIDATE $? "Starting MySQL Server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOG_FILE_NAME
+mysql -h $IP -u root -pExpenseApp@1  # check whether DB connetion is present
+
+if [ $? -ne 0 ]
+then
+    echo "MySQL Root password not setup" &>>$LOG_FILE_NAME
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    VALIDATE $? "Setting Root Password"
+else
+    echo -e "MySQL Root password already setup ... $Y SKIPPING $N"
+fi
